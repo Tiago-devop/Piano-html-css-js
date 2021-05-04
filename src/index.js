5c8c0c13f205b5ee3e39eafee20ec395b8a7274d
Tiago-devop <71716599+Tiago-devop@users.noreply.github.com>
2021-05-04 19:46:11 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="keyboard">
+  <div class="octave">
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="white"></div>
+  </div>
+</div>
+`;
