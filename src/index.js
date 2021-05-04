77a11e4c631b00ed2fd23df69091dad108ffa88c
Tiago-devop <71716599+Tiago-devop@users.noreply.github.com>
2021-05-04 19:53:57 -0300

    #02 element class

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <div class="keyboard">
   <div class="octave">
-    <div class="white"></div>
-    <div class="black"></div>
-    <div class="white"></div>
-    <div class="black"></div>
-    <div class="white"></div>
-    <div class="white"></div>
+    <div class="white" c></div>
+    <div class="black" cs></div>
+    <div class="white" d></div>
+    <div class="black" ds></div>
+    <div class="white" e></div>
+    <div class="white" f></div>
   </div>
 </div>
 `;
