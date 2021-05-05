c28502f9dee55d0168cc70ed5ef4ea76ba2a02d5
Tiago-devop <71716599+Tiago-devop@users.noreply.github.com>
2021-05-04 21:11:53 -0300

    #08 refactoring

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 const keys = document.querySelectorAll(".octave > div");
-console.log(keys);
+
+function unmark() {
+  console.log("unmark");
+}
+
+keys.forEach(function (key) {
+  key.onmousedown = function () {
+    console.log("mark");
+  };
+
+  key.onmouseup = () => unmark(key);
+  key.onmouseleave = () => unmark(key);
+});
